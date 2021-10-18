62393783c4421085631e88f7b32f3d88e14335d2
ValeGian <46gianninivalerio@gmail.com>
2021-10-18 18:18:40 +0200

    Test non-tuned classifiers

--- visualize.py
+++ visualize.py
@@ -25,6 +25,38 @@ def correlation_matrix(data):
 
     plt.savefig('./imgs/audio_features_correlation_matrix.png')
 
+# Helper to plot confusion matrix -- from Scikit-learn website
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.figure(figsize=(10,10))
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
 def waveform(x: any, sr: any = 22050):
     plt.figure(figsize=(14, 5))
     librosa.display.waveplot(x, sr=sr)
